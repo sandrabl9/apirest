18d0ed7acb425939a9058241b01f5ef438eaf660
Sandra Bretones <sandrabretones@gmail.com>
2022-02-23 11:49:36 +0100

    1.1

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get('/api/phones/:id', (request, response) => {
 });
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
