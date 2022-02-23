fdd81515d8b8153727b919240f0ce3347a44be06
Sandra Bretones <sandrabretones@gmail.com>
2022-02-23 16:35:45 +0100

    2.0

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors())
 let phones = [
     {
         "id": 1,
-        "product image":"https://res.cloudinary.com/sbl9/image/upload/v1645542596/phone1_sp0bfc.jpg",
+        "image":"https://res.cloudinary.com/sbl9/image/upload/v1645542596/phone1_sp0bfc.jpg",
         "title": "Iphone 1",
         "description": "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
         "color": "Blue",
@@ -15,7 +15,7 @@ let phones = [
     },
     {
         "id": 2,
-        "product image":"https://res.cloudinary.com/sbl9/image/upload/v1645542596/phone2_hi6pwh.jpg",
+        "image":"https://res.cloudinary.com/sbl9/image/upload/v1645542596/phone2_hi6pwh.jpg",
         "title": "Iphone 2",
         "description": "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         "color": "Black",
@@ -23,7 +23,7 @@ let phones = [
     },
     {
         "id": 3,
-        "product image":"https://res.cloudinary.com/sbl9/image/upload/v1645542596/phone3_b1a21f.jpg",
+        "image":"https://res.cloudinary.com/sbl9/image/upload/v1645542596/phone3_b1a21f.jpg",
         "title": "Iphone 3",
         "description": "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         "color": "White",
@@ -31,7 +31,7 @@ let phones = [
     },
     {
         "id": 4,
-        "product image":"https://res.cloudinary.com/sbl9/image/upload/v1645542596/phone4_dshwu9.jpg",
+        "image":"https://res.cloudinary.com/sbl9/image/upload/v1645542596/phone4_dshwu9.jpg",
         "title": "Iphone 4",
         "description": "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         "color": "Gray",
